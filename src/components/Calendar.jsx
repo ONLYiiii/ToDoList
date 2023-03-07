6bace512ce5a7102be3faaeef26a803ed5c55c81
MaiWaiLaw <earth45689@gmail.com>
2023-03-07 21:01:58 +0700

    Update Create Activity

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,29 +3,22 @@ import { useState, useEffect } from "react";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 import { useData } from '../hooks'
-import { getFormatDateFromCalendar } from "../utils/date";
+import { getFormatDateFromCalendar, getMonthText } from "../utils/date";
 
 const Calendar = ({showCalendar,onShowCalenderChange}) => {
-  const {mock, setMock} = useData()
+  const {newDate, setNewDate} = useData()
 
   const handleConfirm = (date) => {
-    console.log("A date has been picked: ",  getFormatDateFromCalendar(date));
+    // console.log("A date has been picked: ",  getFormatDateFromCalendar(date));
+    setNewDate({
+      day: getFormatDateFromCalendar(date).split('-')[2],
+      monthText: getMonthText(getFormatDateFromCalendar(date).split('-')[1]-1),
+      monthNum: getFormatDateFromCalendar(date).split('-')[1],
+      year: getFormatDateFromCalendar(date).split('-')[0],
+      fullDate: getFormatDateFromCalendar(date),
+    })
     onShowCalenderChange()
   };
-
-  // useEffect(() => {
-  //   if (selectedDate != "") {
-  //     setMock({
-  //       date: {
-  //         day: selectedDate.split('-')[2],
-  //         monthText: getMonthText(Number(selectedDate.split('/')[1]-1)),
-  //         monthNum: selectedDate.split('/')[1],
-  //         year: selectedDate.split('/')[0],
-  //         fullDate: selectedDate,
-  //       }
-  //     })
-  //   }
-  // }, [selectedDate])
 
   return (
     <DateTimePickerModal
